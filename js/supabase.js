7767e9436528cf94ac38e4a8c8d2880d8cf88903
Deeenchick <48181045+Deeenchick@users.noreply.github.com>
2026-07-28 12:01:05 +0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -4,6 +4,6 @@
 // ================================================================
 
 const SUPABASE_URL = 'https://obmhexrwltdtldjdeuxo.supabase.co'
-const SUPABASE_ANON_KEY = 'sb_publishable_HX4idr3fjs1BvNTwZidseg_oiQddbPX'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ibWhleHJ3bHRkdGxkamRldXhvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUxMzY1MDYsImV4cCI6MjEwMDcxMjUwNn0.JcReg2I4AgyVzxOtc9g5wo72250fW_MVJgeH6XjkFbE'
 
 export const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
